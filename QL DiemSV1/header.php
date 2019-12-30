e2c3ed93e2e869b66c67f65bfbc57d2900e99398
Huongltd <57879813+Huongltd@users.noreply.github.com>
2019-12-30 10:32:09 +0700

    co connect

---
+++ QL DiemSV1/header.php
@@ -0,0 +1,31 @@
+    <header>
+        <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+            <div class="container-fluid">
+                <div class="logo">
+                    <img src="./images/logo.png" class="img-fluid" alt="">
+                </div>     
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto" style="font-size: 24px;">
+                  <li class="nav-item">
+                    <a class="nav-link active" href="trangchu.php" target="">Trang chủ</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="login.php" target="">Đăng nhập</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="tracuudiem.php">Tra cứu điểm</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Hỏi đáp</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Hỗ trợ</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </header>
